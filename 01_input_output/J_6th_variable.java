c51973483b46f5311ee4f9915ff937e5f2500c97
Riku Sahu <riku.sahu237@gmail.com>
2025-11-13 19:09:22 +0530

    Add files via upload

---
+++ 01_input_output/J_6th_variable.java
@@ -0,0 +1,13 @@
+public class J_4th_program {
+   public static void main(String[] args) {
+      // variabl
+      int a = 2;
+      int b = 3;
+      int sum = a + b;
+      System.out.println(sum);
+      int sub = b - a;
+      System.out.println(sub);
+      int mul = a * b;
+      System.out.println(mul);
+   }
+}
