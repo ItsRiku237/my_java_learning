2597444d846d4ee2e27c59931babb33688e41bf9
Riku Sahu <riku.sahu237@gmail.com>
2025-11-13 19:05:32 +0530

    Add files via upload

---
+++ 01_input_output/J_02_PrintVsPrintln.java.java
@@ -0,0 +1,8 @@
+public class J_02_PrintVsPrintln {
+    public static void main(String[] args){
+        //Output
+        System.out.println("Riku");// make a new line after this print
+        System.out.print("Riku\n");// This also work as println
+        System.out.print("Riku"); //it does not make new line simply print next to it in same line.
+    }
+}
---
+++ 01_input_output/J_03_StarPattern_Basic.java.java
@@ -0,0 +1,9 @@
+public class J_03_StarPattern_Basic {
+    public static void main(String arg[]) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        // System.out.print("*\n* *\n* * *\n* * * *\n");// this is Same as upper output
+    }
+}
