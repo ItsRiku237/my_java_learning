21ffb36ac166ba36c8417cbc3a925d06c59505b2
Riku Sahu <riku.sahu237@gmail.com>
2025-11-16 22:05:30 +0530

    Add files via upload

---
+++ 03_loops/J_03_MultiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class J_03_MultiplicationTable {
+    // Print the table of a number input by the user .
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n * i);
+        }
+    }
+}
