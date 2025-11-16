65ebd9af8e9fb0ede3116e92718f7c7a4c88c1dd
Riku Sahu <riku.sahu237@gmail.com>
2025-11-16 18:44:45 +0530

    Add files via upload

---
+++ 02_conditional_statements/J_10_InfiniteLoop.java.java
@@ -0,0 +1,10 @@
+public class J_52th_program {
+    // 6. Write an infinite loop using do while condition.
+    public static void main(String[] args) {
+
+        do {
+            System.out.println("Riku");
+        } while (1 > 0);
+
+    }
+}
