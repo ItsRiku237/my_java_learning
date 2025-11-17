88bc7ac5d816d0e6dd74d6656919ac316323778e
Riku Sahu <riku.sahu237@gmail.com>
2025-11-17 19:14:33 +0530

    Add files via upload

---
+++ 03_loops/J_05_InfiniteForLoops.java
@@ -0,0 +1,18 @@
+public class J_05_InfiniteForLoops {
+    public static void main(String[] args) {
+
+//Below all for loops are infinite  for loops.
+
+        for (;;) {
+            System.out.println("Riku");
+        }
+
+        // for (int i = 0;; i++) {
+        //     System.out.println("Riku");
+        // }
+
+        // for (int i = 0; i < 10; i--) {
+        //     System.out.println("Riku");
+        // }
+    }
+}
