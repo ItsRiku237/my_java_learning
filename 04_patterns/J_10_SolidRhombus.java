aab5a02c6edbb8ba182720e58b4d32cf9dfbaf0b
Riku Sahu <riku.sahu237@gmail.com>
2025-11-18 19:25:35 +0530

    Add files via upload

---
+++ 04_patterns/J_10_SolidRhombus.java
@@ -0,0 +1,25 @@
+public class J_10_SolidRhombus {
+    // Homework Problems
+    // 11. Print a solid rhombus.
+    //         * * * * *
+    //       * * * * *
+    //     * * * * *
+    //   * * * * *
+    // * * * * *
+    public static void main(String[] args) {
+        int n = 5;
+
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop  -> print space
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("  ");
+            }// print stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
