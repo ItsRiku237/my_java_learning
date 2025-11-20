c819fc672c15b11492358d28210397b14c83f819
Riku Sahu <riku.sahu237@gmail.com>
2025-11-20 19:24:27 +0530

    Add files via upload

---
+++ 04_patterns/J_19_HollowRhombus.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class J_19_HollowRhombus {
+    // 19. Print a Hallow rohombs .(n=5)
+    //     *****
+    //    *   *
+    //   *   *
+    //  *   *
+    // *****
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            if (i == 1 || i == n) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            } else {
+
+                for (int j = 1; j <= n; j++) {
+                    if (j == 1 || j == n) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
