52b9da5cd5b9804251d8e7a7f22eb09f6058b67d
Riku Sahu <riku.sahu237@gmail.com>
2025-11-22 23:26:02 +0530

    Add files via upload

---
+++ 05_methods_functions/J_15_fibonacci_series.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class J_10_fibonacci_series {
+    // 10. Write a program to print Fibonacci series of n terms where n is input
+    // by user :
+    // 0 1 1 2 3 5 8 13 21 .....
+    // In the Fibonacci series, a number is the sum of the previous 2 numbers that
+    // came before it.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of term :");
+        int n = sc.nextInt();
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        sc.close();
+    }
+}
