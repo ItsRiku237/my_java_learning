1d1e6da599ace34e439c4fb2dcfafd6afe847a4e
Riku Sahu <riku.sahu237@gmail.com>
2025-11-30 21:20:30 +0530

    Add files via upload

---
+++ 12_oops/J_19_package_demo.java
@@ -0,0 +1,15 @@
+/*MyJavaProject/
+   src/
+      Riku/
+        J_18_package
+    J_19_package_dem
+   bin/
+ */
+import Riku.*;
+
+public class J_19_package_demo {
+    public static void main(String args[]){
+        J_18_package R = new J_18_package();
+        R.myfunction();
+    }
+}
